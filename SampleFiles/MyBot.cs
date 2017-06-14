454f976d6bb62c2b91e69e26544b6adb3c013623
BuildTools <steven.greens10@gmail.com>
2017-06-14 18:17:28 -0400

    Update visualizer

---
+++ SampleFiles/MyBot.cs
@@ -0,0 +1,31 @@
+﻿using CSharpRunner;
+
+namespace testBot1
+{
+    public class MyBot : IMazeBot
+    {
+        public void DoTurn(Game game)
+        {
+            try
+            {
+                if (game.GetTurn() == 0)
+                {
+                    game.Debug("This is the first turn!");
+                }
+                Cell c = game.GetCurrentCell();
+                for (int i = 0; i < 3; i++)
+                {
+                    if (c.Walls[i])
+                    {
+                        game.Move((Direction)i);
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+                
+            }
+        }
+    }
+}
--- Visualizer/main.js
+++ Visualizer/main.js
@@ -2,12 +2,12 @@ var cells;
 var player;
 
 function setup(){
-  createCanvas(1001,1001);
+  createCanvas(501,501);
   cells = [];
   player = undefined;
   refreshData();
   noLoop();
-  setInterval(refreshData, 5000);
+  //setInterval(refreshData, 5000);
 }
 
 function draw(){
@@ -16,33 +16,34 @@ function draw(){
   stroke(255);
   for(var i = 0; i < cells.length; i++){
     var cell = cells[i];
-    var scl = 10;
+    var scl = 5;
     var x = cell.x*scl;
     var y = cell.y*scl;
     //Top left to top right
-    if(cell.Walls[0]) line(x,y,x+scl,y);
+    if(cell.Walls[0] === false) line(x,y,x+scl,y);
+
     //Top right to bottom right
-    if(cell.Walls[1]) line(x+scl,y,x+scl,y+scl);
+    if(cell.Walls[1] === false) line(x+scl,y,x+scl,y+scl);
     //Bottom right to bottom left
-    if(cell.Walls[2]) line(x+scl,y+scl,x,y+scl);
+    if(cell.Walls[2] === false) line(x+scl,y+scl,x,y+scl);
     //Bottom left to top left
-    if(cell.Walls[3]) line(x,y+scl,x,y);
+    if(cell.Walls[3] === false) line(x,y+scl,x,y);
   }
 
 
 }
 
 function refreshData(){
-  loadJSON('./mazedata.json', function(data){
+	var data = JSON.parse(document.getElementById("maze").innerText);
     for(var i = 0; i < data.Cells.length; i++){
       for(var j = 0; j < data.Cells[i].length; j++){
         cells.push(data.Cells[i][j]);
       }
     }
     redraw();
-  });
+  
 
-  loadJSON('./playerdata.json', function(data){
+  /*loadJSON('./playerdata.json', function(data){
     if(data[0].Player) player = data[0].Player;
-  });
+  });*/
 }
