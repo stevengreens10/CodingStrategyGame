ce4ce3e992007a6a6e9260add2c2ab8f681615c8
BuildTools <steven.greens10@gmail.com>
2017-06-14 08:39:58 -0400

    Added player move method

--- Player.cs
+++ Player.cs
@@ -2,5 +2,31 @@
 {
     class Player
     {
+
+        private Cell currentCell;
+
+        public Player(Maze maze)
+        {
+
+        }
+
+        public void Move(Cell c)
+        {
+            Cell[] neighbors = currentCell.getNeighbors();
+            bool isNeighbor = false;
+
+            for(int i = 0; i < neighbors.Length; i++)
+            {
+                if(neighbors[i] == c)
+                {
+                    isNeighbor = true;
+                }
+            }
+
+            if (isNeighbor)
+            {
+                currentCell = c;
+            }
+        }
     }
 }
