4217f00dbf91e1c78057e47ab60044e7587aa651
BuildTools <steven.greens10@gmail.com>
2017-06-14 18:42:52 -0400

    Update visualizer

--- Visualizer/main.js
+++ Visualizer/main.js
@@ -1,22 +1,18 @@
 var cells;
-var player;
+var playerData;
 var turn = 0;
+var scl = 5;
 
 function setup(){
   createCanvas(501,501);
   cells = [];
   playerData = undefined;
   refreshData();
-  //setInterval(refreshData, 5000);
-}
-
-function draw(){
   background(51);
   noFill();
   stroke(255);
   for(var i = 0; i < cells.length; i++){
     var cell = cells[i];
-    var scl = 5;
     var x = cell.x*scl;
     var y = cell.y*scl;
     //Top left to top right
@@ -30,11 +26,20 @@ function draw(){
     if(cell.Walls[3] === false) line(x,y+scl,x,y);
   }
 
+  frameRate(5);
+  //setInterval(refreshData, 5000);
+}
+
+function draw(){
+//  background(51);
+
+
   if(turn >= 1000) turn = 0;
   var player = playerData[turn].Player;
+  console.log("Turn #", playerData[turn].turn, "(",player.Location.X, ",",player.Location.Y,")");
   fill(0,255,0);
   stroke(0);
-  ellipse(player.Location.X * scl, player.Location.Y * scl, scl, scl);
+  ellipse(player.Location.X * scl + scl/2, player.Location.Y * scl + scl/2, scl, scl);
   turn++;
 
 
@@ -48,10 +53,8 @@ function refreshData(){
         cells.push(data.Cells[i][j]);
       }
     }
-    redraw();
 
 
-    data = JSON.parse(document.getElementById("maze").innerText);
+    data = JSON.parse(document.getElementById("player").innerText);
     if(data) playerData = data;
-  });*/
 }
