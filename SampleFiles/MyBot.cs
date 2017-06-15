fd80f8365886d7ad06e527eafd97ee8b431ea342
shaked6540 <shaked6540@users.noreply.github.com>
2017-06-15 03:03:52 +0300

    Hes a bit smarter now, but still dumb as hell

--- SampleFiles/MyBot.cs
+++ SampleFiles/MyBot.cs
@@ -1,31 +1,38 @@
 ﻿using CSharpRunner;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace testBot1
 {
     public class MyBot : IMazeBot
     {
+        static List<Cell> history;
         public void DoTurn(Game game)
         {
-            try
+            if (game.GetTurn() == 0)
             {
-                if (game.GetTurn() == 0)
-                {
-                    game.Debug("This is the first turn!");
-                }
-                Cell c = game.GetCurrentCell();
-                for (int i = 0; i < 3; i++)
-                {
-                    if (c.Walls[i])
-                    {
+                history = new List<Cell>();
+            }
+
+            Cell c = game.GetCurrentCell();
+
+            if (!history.Contains(c)) history.Add(c);
+            if (!c.Walls.Where(x => x).Any())
+            {
+                game.Debug("Im stuck! no walls are open");
+                game.Debug("Cell {c.GetLocation().X} {c.GetLocation().Y}");
+                return;
+            }
+
+            for (int i = 0; i < 3; i++)
+                if (c.Walls[i])
+                    if (!history.Contains(game.CellInDirection((Direction)i)))
                         game.Move((Direction)i);
-                        break;
-                    }
-                }
-            }
-            catch
-            {
-                
-            }
+
+            for (int i = 0; i < 3; i++)
+                if (c.Walls[i])
+                    game.Move((Direction)i);
+
         }
     }
 }
