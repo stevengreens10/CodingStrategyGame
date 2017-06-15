b892e8ea17bb9e9a1569de0c66f8cd037f6b635f
Shaked Dahan <shaked6540@users.noreply.github.com>
2017-06-15 23:32:48 +0300

    Makes the cells bolder the more the player visited them

--- Visualizer/main.js
+++ Visualizer/main.js
@@ -25,7 +25,7 @@ function setup(){
   
   restart.mousePressed(function(){
 	  stopped = false;
-	  turn = 0;
+	  turn=0;
   });
   
   rewind.mousePressed(function(){
@@ -66,9 +66,10 @@ function setup(){
     var cell = cells[i];
     var x = cell.x*scl;
     var y = cell.y*scl;
+	
+	
     //Top left to top right
     if(cell.Walls[0]) line(x,y,x+scl,y);
-
     //Top right to bottom right
     if(cell.Walls[1]) line(x+scl,y,x+scl,y+scl);
     //Bottom right to bottom left
@@ -86,27 +87,31 @@ function draw(){
   frameRate(FPS);
   
   if (!stopped) {
-  //if(turn >= 1000) turn = 0;
-  var player = playerData[turn].Player;
-  var x = player.currentCell.x;
-  var y = player.currentCell.y;
-  console.log("Turn #", playerData[turn].turn, "(",x, ",",y,")");
+	//if(turn >= 1000) turn = 0;
+	var player = playerData[turn].Player;
 
-  fill(0,0,0);
-  stroke(0);
-  ellipse(lastTurnX * scl + scl/2, lastTurnY*scl + scl/2, scl/1.5, scl/1.5);
-  lastTurnX = player.currentCell.x;
-  lastTurnY = player.currentCell.y;
+	var x = player.currentCell.x;
+	var y = player.currentCell.y;
+	console.log("Turn #", playerData[turn].turn, "(",x, ",",y,")");
 
-  fill(0,255,0);
-  ellipse(x * scl + scl/2, y * scl + scl/2, scl/1.5, scl/1.5);
+	fill(0,0,0);
+	stroke(0);
+	
+	var color = player.currentCell.Visited*10;
+	fill(color/1.7, color, color*1.4);
+	rect((lastTurnX * scl)+2, (lastTurnY*scl) + 1,scl-3, scl-2);
+  
+	lastTurnX = player.currentCell.x;
+	lastTurnY = player.currentCell.y;
 
+	fill(0,255,0);
+	ellipse(x * scl + scl/2, y * scl + scl/2, scl/1.5, scl/1.5);
+  
 
-  turn++;
-  }
-
-
+	turn++;
+	}
 }
+
 
 function refreshData(){
 	var data = JSON.parse(document.getElementById("maze").innerText);
